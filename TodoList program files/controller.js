f8b1100962a1ee655eb4da9c91ce4d115961d896
Toanuvo <Kzeezavadlov@yahoo.com>
2020-11-14 17:35:08 -0800

    1 js file, worked on addtask(), colored points
    moved classes into the controller js and modified references so they work. addtask() will now add all the tasks from the list array and clear the input. added color to points and added a number.

--- TodoList program files/controller.js
+++ TodoList program files/controller.js
@@ -1,5 +1,62 @@
-import List from './todolist';
+// eslint-disable-next-line max-classes-per-file
+class Task {
+  constructor(name, date, priority) {
+    this.name = name;
+    this.date = date;
+    this.priority = priority;
+  }
+}
 
-const maintodolist = new List.List();
-maintodolist.addtask('test', 0, 0);
-// reference by id or querySelector??
+class List {
+  constructor() {
+    this.tasks = [];
+  }
+
+  addtask(name, date, priority) {
+    const task = new Task(name, date, priority);
+    this.tasks.push(task);
+  }
+}
+console.log('controller script loaded');
+
+// setup variables and objects
+const addtaskB = document.getElementById('addtask_button');
+const taskdisplay = document.getElementById('tasks');
+const maintodolist = new List();
+let tasknum = 0;
+
+// define controller functions
+function addtask() {
+  tasknum++;
+  const name = document.getElementById('name_input');
+  const date = document.getElementById('date_input');
+  const priority = document.getElementById('priority_input');
+  maintodolist.addtask(name.value, date.value, priority.value);
+
+  for (const t of maintodolist.tasks) {
+    const T = document.createElement('DIV');
+    T.className = 'ui raised segment';
+    T.innerText = t.name;
+    T.id = `task${tasknum}`;
+    const D = document.createElement('DIV');
+    const P = document.createElement('DIV');
+    const sect = document.createElement('DIV');
+    sect.className = 'ui horizontal segments';
+    D.className = 'ui segment';
+    D.innerText = t.date;
+    P.className = 'ui segment';
+    P.innerText = t.priority;
+
+    sect.appendChild(D);
+    sect.appendChild(P);
+    T.appendChild(sect);
+    taskdisplay.appendChild(T);
+  }
+
+  name.value = '';
+  date.value = '';
+  priority.value = '';
+}
+
+// attach listeners
+addtaskB.addEventListener('click', addtask);
