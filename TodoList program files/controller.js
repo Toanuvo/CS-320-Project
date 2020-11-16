1151485cde6beca08e92e024ffba5451c4446c52
Toanuvo <Kzeezavadlov@yahoo.com>
2020-11-16 12:07:39 -0800

    added task description functionality and changed how tasks look

--- TodoList program files/controller.js
+++ TodoList program files/controller.js
@@ -15,12 +15,12 @@ class List {
     this.streak = 0;
   }
 
-  addtask(name, date, priority) {
-    const task = new Task(name, date, priority);
+  addtask(name, date, priority, desc) {
+    const task = new Task(name, date, priority, desc);
     this.tasks.push(task);
   }
 
-  edittask(tasknum, name, date, desc, priority) {
+  edittask(tasknum, name, date, priority, desc) {
     const task = this.tasks[tasknum];
     task.name = name;
     task.date = date;
@@ -45,7 +45,7 @@ class List {
 }
 console.log('controller loaded');
 
-// todo subtasks, add task desc functionality (use text area?), task sorting
+// todo subtasks, task sorting
 
 // setup variables and objects
 const addtaskB = document.getElementById('addtask_button');
@@ -56,6 +56,7 @@ const pointsdisplay = document.getElementById('points');
 const name = document.getElementById('name_input');
 const date = document.getElementById('date_input');
 const priority = document.getElementById('priority_input');
+const desc = document.getElementById('desc_input');
 
 const maintodolist = new List();
 let curtask = -1;
@@ -65,50 +66,68 @@ function displayTasks() {
   taskdisplay.innerHTML = '';
   let tempnum = 0;
   for (const t of maintodolist.tasks) {
-    const T = document.createElement('DIV');
-    T.className = 'ui raised segment';
-    T.innerText = t.name;
-    T.id = `task${tempnum}`;
-    const D = document.createElement('DIV');
-    const P = document.createElement('DIV');
+    const TASK = document.createElement('DIV');
+    TASK.className = 'ui raised segment';
+    TASK.id = `task${tempnum}`;
+    const NAME = document.createElement('DIV');
+    const DATE = document.createElement('DIV');
+    const PRIORITY = document.createElement('DIV');
     const sect = document.createElement('DIV');
-    const B = document.createElement('BUTTON');
-    const C = document.createElement('BUTTON');
+    const EDIT_B = document.createElement('BUTTON');
+    const COMPLETE_B = document.createElement('BUTTON');
+    const DESC = document.createElement('DIV');
+
     sect.className = 'ui horizontal segments';
-    D.className = 'ui segment';
-    D.innerText = t.date;
-    P.className = 'ui segment';
-    P.innerText = t.priority;
-    B.className = 'ui button';
-    B.id = `edittask${tempnum}`;
-    B.innerText = 'Edit Task';
-    C.className = 'ui button';
-    C.id = `completetask${tempnum}`;
-    C.innerText = 'Complete Task';
+    NAME.className = 'ui segment';
+    NAME.innerText = t.name;
+    DATE.className = 'ui segment';
+    DATE.innerText = t.date;
+    PRIORITY.className = 'ui segment';
+    PRIORITY.innerText = t.priority;
+    DESC.className = 'ui raised segment';
+    DESC.innerText = t.desc;
 
-    // edit task function
-    B.addEventListener('click', editTask.bind(this, t));
-    C.addEventListener('click', completeTask.bind(this, t));
+    EDIT_B.className = 'ui button';
+    EDIT_B.id = `edittask${tempnum}`;
+    EDIT_B.innerText = 'Edit Task';
+    COMPLETE_B.className = 'ui button';
+    COMPLETE_B.id = `completetask${tempnum}`;
+    COMPLETE_B.innerText = 'Complete Task';
 
-    sect.appendChild(D);
-    sect.appendChild(P);
-    T.appendChild(sect);
-    T.appendChild(B);
-    T.appendChild(C);
-    taskdisplay.appendChild(T);
+    // add listeners to buttons
+    EDIT_B.addEventListener('click', editTask.bind(this, t));
+    COMPLETE_B.addEventListener('click', completeTask.bind(this, t));
+
+    sect.appendChild(NAME);
+    sect.appendChild(DATE);
+    sect.appendChild(PRIORITY);
+    TASK.appendChild(sect);
+    TASK.appendChild(DESC);
+    TASK.appendChild(EDIT_B);
+    TASK.appendChild(COMPLETE_B);
+    taskdisplay.appendChild(TASK);
     tempnum++;
   }
-
   pointsdisplay.innerText = maintodolist.points;
+}
+
+function clearInput() {
+  name.value = '';
+  date.value = '';
+  priority.value = '';
+  desc.value = '';
+  document.getElementById('task_editor_title').innerText = 'Inputs for a new task';
+  curtask = -1;
+  addtaskB.innerText = 'Add Task';
 }
 
 function addTask() {
   if (curtask !== -1) {
-    maintodolist.edittask(curtask, name.value, date.value, '', priority.value);
+    maintodolist.edittask(curtask, name.value, date.value, priority.value, desc.value);
     curtask = -1;
     document.getElementById('task_editor_title').innerText = 'Inputs for a new task';
   } else {
-    maintodolist.addtask(name.value, date.value, priority.value);
+    maintodolist.addtask(name.value, date.value, priority.value, desc.value);
   }
   displayTasks();
   clearInput();
@@ -120,6 +139,7 @@ function editTask(task) {
   name.value = task.name;
   date.value = task.date;
   priority.value = task.priority;
+  desc.value = task.desc;
   document.getElementById('task_editor_title').innerText = `editing task ${task.name}`;
   addtaskB.innerText = `reconfigure ${task.name}`;
 }
@@ -137,15 +157,6 @@ function deleteTask() {
 function completeTask(task) {
   maintodolist.compeltetask(task);
   displayTasks();
-}
-
-function clearInput() {
-  name.value = '';
-  date.value = '';
-  priority.value = '';
-  document.getElementById('task_editor_title').innerText = 'Inputs for a new task';
-  curtask = -1;
-  addtaskB.innerText = 'Add Task';
 }
 
 // attach listeners
