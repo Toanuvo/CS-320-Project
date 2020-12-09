8fefe4aab111fabb394e407dfab919b121e94837
RileyRat5x <35906066+RileyRat5x@users.noreply.github.com>
2020-12-08 21:14:44 -0800

    first test implemented for list add task
    one task is added and is checked if it has all required elements

--- app/mochatests.js
+++ app/mochatests.js
@@ -2,10 +2,15 @@
 
 
 describe("todolist tests", function (){
+  let list = new List()
   describe("List class functions", function(){
     describe("addtask()", function (){
       it('should take name date pirority desc and task number and add a task to the list', function () {
-        chai.expect()
+        list.addtask("test task", new Date("01/12/2021"), 0, "this is a test")
+        chai.expect(list.tasks[0].name).to.equal("test task")
+        chai.expect(list.tasks[0].date).to.deep.equal(new Date("01/12/2021"))
+        chai.expect(list.tasks[0].priority).to.equal(0)
+        chai.expect(list.tasks[0].desc).to.equal("this is a test")
 
       });
     })
