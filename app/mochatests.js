bd3962b4562c6bb4eac5a24356e942239d5baf4f
RileyRat5x <35906066+RileyRat5x@users.noreply.github.com>
2020-12-08 21:29:56 -0800

    added all blank test functions for list functions
    added edittask, completetask, removetask, changeusername, resetstreak, resetpoints, sortaz, sortza, sort01, sort10, sortdate

--- app/mochatests.js
+++ app/mochatests.js
@@ -2,10 +2,11 @@
 
 
 describe("todolist tests", function (){
-  let list = new List()
+
   describe("List class functions", function(){
     describe("addtask()", function (){
       it('should take name date pirority desc and task number and add a task to the list', function () {
+        let list = new List()
         list.addtask("test task", new Date("01/12/2021"), 0, "this is a test")
         chai.expect(list.tasks[0].name).to.equal("test task")
         chai.expect(list.tasks[0].date).to.deep.equal(new Date("01/12/2021"))
@@ -13,8 +14,49 @@ describe("todolist tests", function (){
         chai.expect(list.tasks[0].desc).to.equal("this is a test")
 
       });
+    });
+    describe("edittask()",function (){
+
+    })
+    describe("compltetask()",function (){
+
+    })
+    describe("removeTask()",function (){
+
     })
 
-  })
+    describe("changeUsername()", function (){
 
-})
+
+    });
+    describe("resetStreak()", function (){
+
+    })
+    describe("resetPoints()",function(){
+
+    })
+    describe("sortAZ()",function ()){
+
+    }
+    describe("sortZA()",function ()){
+
+    }
+    describe("sort01()",function ()){
+
+    }
+    describe("sort10()",function ()){
+
+    }
+    describe("sortDate()",function ()){
+
+    }
+
+
+
+
+
+
+
+  });
+
+});
