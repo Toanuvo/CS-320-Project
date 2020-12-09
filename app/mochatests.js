6c2394b22d34a95b6622bdadc3d74cd03d30fae1
RileyRat5x <35906066+RileyRat5x@users.noreply.github.com>
2020-12-08 20:55:54 -0800

    added files for mocha chai tests
    added mochatests.js and tests.html

---
+++ app/mochatests.js
@@ -0,0 +1,15 @@
+/* global  addTask, deleteTask, addSubTasks,  , resePoints, resetStreak, changeUiColor, changeFont,  */
+
+
+describe("todolist tests", function (){
+  describe("List class functions", function(){
+    describe("addtask()", function (){
+      it('should take name date pirority desc and task number and add a task to the list', function () {
+        chai.expect()
+
+      });
+    })
+
+  })
+
+})
