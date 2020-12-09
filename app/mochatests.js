cf22b9fbe3bb14459fc72f9ff7f3485ba02cff38
RileyRat5x <35906066+RileyRat5x@users.noreply.github.com>
2020-12-08 21:42:18 -0800

    added all it checks
    added all it checks that at the moment we think we need

--- app/mochatests.js
+++ app/mochatests.js
@@ -14,40 +14,91 @@ describe("todolist tests", function (){
         chai.expect(list.tasks[0].desc).to.equal("this is a test")
 
       });
+      it('should beable to add multiple tasks and are in the order they were put in ', function () {
+        
+      });
+      it('should beable to add subtasks to tasks', function () {
+        
+      });
+      it('should beable to add many subtasks to tasks', function () {
+        
+      });
     });
     describe("edittask()",function (){
+      it('should be able to edit a given task given an index for the task', function () {
+        
+      });
+      it('should be able to edit subtask', function () {
+        
+      });
 
     })
     describe("compltetask()",function (){
-
+      it('should be able to complete tasks as well as update streak and points', function () {
+        
+      });
+      it('should be able to reset streak if a task is overdue', function () {
+        
+      });
+      it('should be able to add more points if a streak is going ', function () {
+        
+      });
     })
     describe("removeTask()",function (){
+      it('should be able to remove tasks', function () {
+        
+      });
+      it('should be able to reamove a subtask', function () {
+        
+      });
 
     })
 
     describe("changeUsername()", function (){
-
+      it('should be able to change the username', function () {
+        
+      });
 
     });
     describe("resetStreak()", function (){
+      it('should be able to reset the current streak', function () {
+        
+      });
 
     })
     describe("resetPoints()",function(){
+      it('should be able to reset the current points to zero', function () {
+        
+      });
 
     })
     describe("sortAZ()",function ()){
+      it('should be able to sort the current list of tasks by alphabetal order', function () {
+        
+      });
 
     }
     describe("sortZA()",function ()){
+      it('should be able to sort the current list of tasks by reverse alphabetical order', function () {
+        
+      });
 
     }
     describe("sort01()",function ()){
+      it('should be able to sort the current list of tasks by priority lowest first', function () {
+        
+      });
 
     }
     describe("sort10()",function ()){
-
+      it('should be able to sort the current list of tasks by priority highest first', function () {
+        
+      });
     }
     describe("sortDate()",function ()){
+      it('should beable to sort the current list of tasks by date earliest first', function () {
+        
+      });
 
     }
 
